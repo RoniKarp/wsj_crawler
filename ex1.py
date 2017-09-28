594fd232ce6f9222673b79af89f9ac4296a47a16
ronikarp@gmail.com <R2o5n7iG>
2017-09-28 22:48:21 +0300

    wsj_scraping small refactor

--- ex1.py
+++ ex1.py
@@ -1,12 +1,13 @@
 from utils import scraping_data_from_wsj_by_year
 import shutil
 
+
 shutil.rmtree('ex1_result')
 from_year = input("Enter From Year(yyyy):")
 to_year = input("Enter To Year(yyyy):")
-year_list=range(int(from_year),int(to_year))
+year_list=range(int(from_year),int(to_year)+1)
 print("Start analyses..")
 for year in year_list:
     print(year)
-    data_file = scraping_data_from_wsj_by_year("headline_spider.py", year, 12,'result/temp/ex1_scraped_data.csv')
+    data_file = scraping_data_from_wsj_by_year("article_info_spider.py", year, 12,'result/temp/ex1_scraped_data.csv')
 print("Scraping complete..")
